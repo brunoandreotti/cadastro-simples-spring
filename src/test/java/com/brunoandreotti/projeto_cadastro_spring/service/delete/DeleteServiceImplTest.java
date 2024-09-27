654acdbe8878cc123517c0cfe2dede0d9bd37d8a
Bruno Andreotti <brunoandreotti@outlook.com>
2024-09-27 17:26:52 -0300

    testeee

--- src/test/java/com/brunoandreotti/projeto_cadastro_spring/service/delete/DeleteServiceImplTest.java
+++ src/test/java/com/brunoandreotti/projeto_cadastro_spring/service/delete/DeleteServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.*;
@@ -34,14 +35,30 @@ public class DeleteServiceImplTest {
             verify(userPJRepository, times(1)).deleteById(validId);
         }
 
+        @Test
+        public void test_deletePJ_successful_deletion_2() {
+            UserPJRepository userPJRepository = Mockito.mock(UserPJRepository.class);
+            DeleteServiceImpl deleteService = new DeleteServiceImpl(null, userPJRepository);
+
+            Long validId = 1L;
+            Mockito.when(userPJRepository.existsById(validId)).thenReturn(true);
+
+            deleteService.deletePJ(validId);
+
+            Mockito.verify(userPJRepository, Mockito.times(1)).deleteById(validId);
+        }
+
     @Test
-    public void test_deletePJ_successful_deletion_2() {
+    public void test_deletePJ_successful_deletion_3() {
+        UserPJRepository userPJRepository = Mockito.mock(UserPJRepository.class);
+        DeleteServiceImpl deleteService = new DeleteServiceImpl(null, userPJRepository);
+
         Long validId = 1L;
-        doNothing().when(userPJRepository).deleteById(validId);
+        Mockito.when(userPJRepository.existsById(validId)).thenReturn(true);
 
         deleteService.deletePJ(validId);
 
-        verify(userPJRepository, times(1)).deleteById(validId);
+        Mockito.verify(userPJRepository, Mockito.times(1)).deleteById(validId);
     }
 
 }
